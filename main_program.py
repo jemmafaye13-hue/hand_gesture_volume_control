d9324533b16578536835c912715c6c4fa6500d07
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-02 16:03:57 +0800

    Added RGB conversion for hand detection

--- main_program.py
+++ main_program.py
@@ -9,6 +9,7 @@ drawing_utils = mp.solutions.drawing_utils
 while True:
     _ , image = webcam.read()
     cv2.imshow("Hand volume control using python", image)
+    rgb_image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
     key = cv2.waitKey(10)
     if key == 27:
         break

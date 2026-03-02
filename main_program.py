ec02ee1f86ebf0e40fec419b939645b6c42fbaac
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-02 18:57:52 +0800

    Refactored variable names for better readability and added volume control git push origin main
    '

--- main_program.py
+++ main_program.py
@@ -8,6 +8,9 @@ mp_drawing = mp.solutions.drawing_utils
 webcam = cv2.VideoCapture(0)
 hands_detector = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
 
+thumb_x, thumb_y = 0, 0
+index_x, index_y = 0, 0
+
 while True:
     success, image = webcam.read()
     if not success: break
@@ -22,21 +25,21 @@ while True:
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
             landmarks = hand_landmarks.landmark
-            for id, landmark in enumerate(landmarks):
-                x = int(landmark.x * image.shape[1])
-                y = int(landmark.y * image.shape[0])
+            for finger_id, landmark in enumerate(landmarks):
+                height, width, _ = image.shape
+                coord_x, coord_y = int(landmark.x * width), int(landmark.y * height)
 
-                if id == 8:
-                    cv2.circle(img=image, center=(x, y), radius=10, color=(0, 255, 255))
-                    x8, y8 = x, y
+                if finger_id == 8:
+                    cv2.circle(image, (coord_x, coord_y), 10, (255, 0, 255), cv2.FILLED)
+                    thumb_x, thumb_y = coord_x, coord_y
 
-                if id == 4:
+                if finger_id == 4:
                     cv2.circle(img=image, center=(x, y), radius=10, color=(0, 255, 255))
                     x4, y4 = x, y
 
-            dist = ((x8 - x4) ** 2 + (y8 - y4) ** 2) ** 0.5
+            finger_distance = ((index_x - thumb_x) ** 2 + (index_y - thumb_y) ** 2) ** 0.5
 
-            if dist > 50:
+            if finger_distance > 50:
                 pyautogui.press("volumeup")
             else:
                 pyautogui.press("volumedown")

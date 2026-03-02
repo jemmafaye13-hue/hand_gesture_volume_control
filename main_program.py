2a9d8830d0c7b0b43ce583df4f2d891633ad98a2
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-02 15:47:02 +0800

    Fixed indentation and added escape key logic

--- main_program.py
+++ main_program.py
@@ -6,4 +6,8 @@ webcam = cv2.VideoCapture(0)
 while True:
     _ , image = webcam.read()
     cv2.imshow("Hand volume control using python", image)
-    
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
